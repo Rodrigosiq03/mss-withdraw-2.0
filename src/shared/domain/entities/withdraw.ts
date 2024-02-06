de8658541095ef431ed2b28a825c882d125e362e
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-06 15:50:29 -0300

    starting entity without getters

--- src/shared/domain/entities/withdraw.ts
+++ src/shared/domain/entities/withdraw.ts
@@ -7,3 +7,32 @@ export interface WithdrawProps {
   withdrawalTime: number
   finishTime?: number
 }
+
+export class Withdraw {
+  constructor(public props: WithdrawProps) {
+    if (!Withdraw.validateWithdrawId(props.withdrawId)) {
+      throw new EntityError('withdrawId')
+    }
+
+    if (!Withdraw.validateNotebookSerialNumber(props.notebookSerialNumber)) {
+      throw new EntityError('notebookSerialNumber')
+    }
+
+    if (!Withdraw.validateStudentRA(props.studentRA)) {
+      throw new EntityError('studentRA')
+    }
+
+    if (!Withdraw.validateTime(props.withdrawalTime)) {
+      throw new EntityError('withdrawalTime')
+    }
+
+    if (
+      props.finishTime !== undefined &&
+      !Withdraw.validateTime(props.finishTime)
+    ) {
+      throw new EntityError('finishTime')
+    }
+  }
+
+  
+}
