0ed011919f508d664d7d45fd7fc0d25dab298d46
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-12 12:46:50 -0300

    fixing withdraw test

--- tests/shared/domain/entities/withdraw.test.ts
+++ tests/shared/domain/entities/withdraw.test.ts
@@ -19,13 +19,22 @@ describe('[Withdraw Entity Tests]', () => {
   it('Assert Withdraw Entity has an error when withdrawId is invalid', () => {
     expect(() => {
       new Withdraw({
-        withdrawId: '', 
+        withdrawId: '',
         notebookSerialNumber: 'ABC123',
         studentRA: '23.00335-9',
         initTime: Date.now(),
         state: STATE.PENDING,
       })
     }).toThrowError(EntityError)
+    expect(() => {
+      new Withdraw({
+        withdrawId: '',
+        notebookSerialNumber: 'ABC123',
+        studentRA: '23.00335-9',
+        initTime: Date.now(),
+        state: STATE.PENDING,
+      })
+    }).toThrowError('Field props.withdrawId is not valid')
   })
 
   it('Assert Withdraw Entity has an error when notebookSerialNumber is invalid', () => {
@@ -38,6 +47,15 @@ describe('[Withdraw Entity Tests]', () => {
         state: STATE.PENDING,
       })
     }).toThrowError(EntityError)
+    expect(() => {
+      new Withdraw({
+        withdrawId: '1',
+        notebookSerialNumber: '',
+        studentRA: '23.00335-9',
+        initTime: Date.now(),
+        state: STATE.PENDING,
+      })
+    }).toThrowError('Field props.notebookSerialNumber is not valid')
   })
 
   it('Assert Withdraw Entity has an error when studentRA is invalid', () => {
@@ -50,9 +68,18 @@ describe('[Withdraw Entity Tests]', () => {
         state: STATE.PENDING,
       })
     }).toThrowError(EntityError)
+    expect(() => {
+      new Withdraw({
+        withdrawId: '1',
+        notebookSerialNumber: 'ABC123',
+        studentRA: '',
+        initTime: Date.now(),
+        state: STATE.PENDING,
+      })
+    }).toThrowError('Field props.studentRA is not valid')
   })
 
-  it('Assert Withdraw Entity has an error when withdrawalTime is invalid', () => {
+  it('Assert Withdraw Entity has an error when initTime is invalid', () => {
     expect(() => {
       new Withdraw({
         withdrawId: '1',
@@ -62,6 +89,15 @@ describe('[Withdraw Entity Tests]', () => {
         state: STATE.PENDING,
       })
     }).toThrowError(EntityError)
+    expect(() => {
+      new Withdraw({
+        withdrawId: '1',
+        notebookSerialNumber: 'ABC123',
+        studentRA: '23.00335-9',
+        initTime: -1,
+        state: STATE.PENDING,
+      })
+    }).toThrowError('Field props.initTime is not valid')
   })
 
   it('Assert Withdraw Entity has no error when finishTime is undefined', () => {
@@ -87,5 +123,15 @@ describe('[Withdraw Entity Tests]', () => {
         state: STATE.PENDING,
       })
     }).toThrowError(EntityError)
+    expect(() => {
+      new Withdraw({
+        withdrawId: '1',
+        notebookSerialNumber: 'ABC123',
+        studentRA: '23.00335-9',
+        initTime: Date.now(),
+        finishTime: -1,
+        state: STATE.PENDING,
+      })
+    }).toThrowError('Field props.finishTime is not valid')
   })
 })
