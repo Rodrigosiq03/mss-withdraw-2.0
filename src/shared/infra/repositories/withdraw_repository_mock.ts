051da974991be18e0d6ba7717ed747ff730c3bec
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-14 01:10:15 -0300

    starting WithdrawRepositoryMock

---
+++ src/shared/infra/repositories/withdraw_repository_mock.ts
@@ -0,0 +1,7 @@
+import { Withdraw } from '../../domain/entities/withdraw'
+import { IWithdrawRepository } from '../../domain/repositories/withdraw_repository_interface'
+import { NoItemsFound } from '../../helpers/errors/usecase_errors'
+
+export class WithdrawRepositoryMock implements IWithdrawRepository {
+    
+}
