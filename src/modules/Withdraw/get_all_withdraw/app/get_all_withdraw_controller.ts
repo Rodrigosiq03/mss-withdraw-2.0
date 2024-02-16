213a1645e7a19a4ca48b7f63fc3a02f10c9dadfe
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-16 11:52:14 -0300

    adjusting get all withdraw controller

--- src/modules/Withdraw/get_all_withdraw/app/get_all_withdraw_controller.ts
+++ src/modules/Withdraw/get_all_withdraw/app/get_all_withdraw_controller.ts
@@ -4,16 +4,17 @@
 import { GetAllWithdrawsUsecase } from './get_all_withdraw_usecase'
 import { GetAllWithdrawViewmodel } from './get_all_withdraw_viewmodel'
 import {
-  LambdaHttpRequest,
-  LambdaHttpResponse,
-} from '../../../../shared/helpers/external_interfaces/http_lambda_requests'
-import {
   OK,
   NotFound,
   BadRequest,
   InternalServerError,
 } from '../../../../shared/helpers/external_interfaces/http_codes'
 import { IRequest } from '@/shared/helpers/external_interfaces/external_interface'
+import { EntityError } from '@/shared/helpers/errors/domain_errors'
+import {
+  MissingParameters,
+  WrongTypeParameters,
+} from '@/shared/helpers/errors/controller_errors'
 
 export class GetAllWithdrawsController {
   constructor(private usecase: GetAllWithdrawsUsecase) {}
@@ -23,16 +24,28 @@ export class GetAllWithdrawsController {
       const withdraws = await this.usecase.execute()
       const viewModel = new GetAllWithdrawViewmodel(withdraws)
       return new OK(viewModel.toJSON())
-    } catch (error: any) {
+    } catch (error) {
+      const err = error as Error
       if (
-        error instanceof NotFound ||
-        error instanceof BadRequest ||
-        error instanceof InternalServerError
+        err instanceof NotFound ||
+        err instanceof BadRequest ||
+        err instanceof InternalServerError
       ) {
-        return error
+        return this.handleKnownErrors(err)
       } else {
-        return new InternalServerError(error.message)
+        return new InternalServerError(err.message)
       }
     }
+  }
+
+  private handleKnownErrors(error: Error) {
+    if (
+      error instanceof MissingParameters ||
+      error instanceof WrongTypeParameters ||
+      error instanceof EntityError
+    ) {
+      return new BadRequest(error.message)
+    }
+    return error
   }
 }
