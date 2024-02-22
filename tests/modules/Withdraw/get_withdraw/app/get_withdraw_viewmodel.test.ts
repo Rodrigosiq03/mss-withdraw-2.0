aba483a819861613cc5fe7561a0e72a3983e8ddf
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-22 00:38:30 -0300

    finishing get withdraw UT ✅

--- tests/modules/Withdraw/get_withdraw/app/get_withdraw_viewmodel.test.ts
+++ tests/modules/Withdraw/get_withdraw/app/get_withdraw_viewmodel.test.ts
@@ -1,13 +1,11 @@
 import { it, expect, describe } from 'vitest'
-import { Withdraw } from '../../../../../src/shared/domain/entities/withdraw' 
-import { STATE } from '../../../../../src/shared/domain/enums/state_enum' 
+import { Withdraw } from '../../../../../src/shared/domain/entities/withdraw'
+import { STATE } from '../../../../../src/shared/domain/enums/state_enum'
 import { WithdrawViewModel } from '../../../../../src/modules/Withdraw/get_withdraw/app/get_withdraw_viewmodel'
-
 
 describe('Assert Get Withdraw viewmodel is correct at all', () => {
   it('Assert the viewmodel is correct at all', () => {
     const withdraw = new Withdraw({
-      withdrawId: '1',
       notebookSerialNumber: 'ABC123',
       studentRA: '23.00335-9',
       initTime: 1704074148000,
@@ -17,13 +15,12 @@ describe('Assert Get Withdraw viewmodel is correct at all', () => {
     const withdrawViewmodel = new WithdrawViewModel(withdraw).toJSON()
 
     expect(withdrawViewmodel).toEqual({
-      'id': '1',
-      'notebookSerialNumber': 'ABC123',
-      'studentRA': '23.00335-9',
-      'initTime': 1704074148000,
-      'state': 'PENDING',
-      'finishTime': undefined,
-      'message': 'Withdraw has been retrieved successfully'
+      notebookSerialNumber: 'ABC123',
+      studentRA: '23.00335-9',
+      name: '',
+      initTime: 1704074148000,
+      state: 'PENDING',
+      message: 'Withdraw has been retrieved successfully',
     })
   })
 })
