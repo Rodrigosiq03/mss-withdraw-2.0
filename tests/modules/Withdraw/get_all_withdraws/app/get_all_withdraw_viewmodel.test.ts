255433e38bf4c2257a65b86c34525c08294f3e42
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-22 01:07:20 -0300

    finishing get all withdraws viewmodel ✅

--- tests/modules/Withdraw/get_all_withdraws/app/get_all_withdraw_viewmodel.test.ts
+++ tests/modules/Withdraw/get_all_withdraws/app/get_all_withdraw_viewmodel.test.ts
@@ -1,46 +1,46 @@
 import { describe, it, expect } from 'vitest'
 import { Withdraw } from '../../../../../src/shared/domain/entities/withdraw'
 import {
-  GetAllWithdrawViewmodel,
-  WithdrawViewmodel,
+  GetAllWithdrawsViewModel,
+  WithdrawViewModel,
 } from '../../../../../src/modules/Withdraw/get_all_withdraw/app/get_all_withdraw_viewmodel'
 import { STATE } from '../../../../../src/shared/domain/enums/state_enum'
 
 describe('Assert Get All Withdraws viewmodel is correct at all', () => {
   const withdraws = [
     new Withdraw({
-      withdrawId: '1',
       notebookSerialNumber: 'ABC123',
       studentRA: '23.00335-9',
+      name: 'John Doe',
       initTime: 1641006000000,
       state: STATE.PENDING,
     }),
     new Withdraw({
-      withdrawId: '2',
       notebookSerialNumber: 'XYZ456',
       studentRA: '78.90123-4',
+      name: 'Jane Smith',
       initTime: 1641006000000,
       state: STATE.APPROVED,
     }),
   ]
 
   it('Should activate GetAllWithdraws viewmodel correctly', async () => {
-    const viewmodel = new GetAllWithdrawViewmodel(withdraws)
+    const viewmodel = new GetAllWithdrawsViewModel(withdraws)
 
     const expected = {
       withdraws: [
         {
-          id: '1',
           notebookSerialNumber: 'ABC123',
           studentRA: '23.00335-9',
+          name: 'John Doe',
           initTime: 1641006000000,
           finishTime: undefined,
           state: STATE.PENDING,
         },
         {
-          id: '2',
           notebookSerialNumber: 'XYZ456',
           studentRA: '78.90123-4',
+          name: 'Jane Smith',
           initTime: 1641006000000,
           finishTime: undefined,
           state: STATE.APPROVED,
@@ -53,12 +53,12 @@ describe('Assert Get All Withdraws viewmodel is correct at all', () => {
   })
 
   it('Should activate Withdraws viewmodel correctly', async () => {
-    const viewmodel = new WithdrawViewmodel(withdraws[0])
+    const viewmodel = new WithdrawViewModel(withdraws[0])
 
     const expected = {
-      id: '1',
       notebookSerialNumber: 'ABC123',
       studentRA: '23.00335-9',
+      name: 'John Doe',
       initTime: 1641006000000,
       finishTime: undefined,
       state: STATE.PENDING,
