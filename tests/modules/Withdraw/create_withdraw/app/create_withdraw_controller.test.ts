b050c1d12c4fac3e93ad18f23e6d9d5945c82d6e
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-22 17:19:45 -0300

    fix and finishing create withdraw ✅

--- tests/modules/Withdraw/create_withdraw/app/create_withdraw_controller.test.ts
+++ tests/modules/Withdraw/create_withdraw/app/create_withdraw_controller.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest'
-import jwt from 'jsonwebtoken'
 import { WithdrawRepositoryMock } from '../../../../../src/shared/infra/repositories/withdraw_repository_mock'
 import { CreateWithdrawUsecase } from '../../../../../src/modules/Withdraw/create_withdraw/app/create_withdraw_usecase'
 import { HttpRequest } from '../../../../../src/shared/helpers/external_interfaces/http_models'
@@ -11,7 +10,9 @@ describe('Assert Create Withdraw controller is correct at all', () => {
     const usecase = new CreateWithdrawUsecase(repo)
     const controller = new CreateWithdrawController(usecase)
 
-    const request = new HttpRequest(undefined, undefined, { notebookSerialNumber: 'ABC123' })
+    const request = new HttpRequest(undefined, undefined, {
+      notebookSerialNumber: 'ABC123',
+    })
 
     const decoded = {
       role: 'STUDENT',
@@ -22,7 +23,9 @@ describe('Assert Create Withdraw controller is correct at all', () => {
     const response = await controller.handle(request, decoded)
 
     expect(response?.statusCode).toEqual(201)
-    expect(response?.body['message']).toEqual('The withdraw was created successfully')
+    expect(response?.body['message']).toEqual(
+      'The withdraw was created successfully',
+    )
   })
 
   it('Should throw 400 statusCode when not pass notebookSerialNumber', async () => {
@@ -30,18 +33,15 @@ describe('Assert Create Withdraw controller is correct at all', () => {
     const usecase = new CreateWithdrawUsecase(repo)
     const controller = new CreateWithdrawController(usecase)
 
-    const user = { name: 'Test User', studentRA: '23.00335-9' }
-    const token = jwt.sign({ user }, jwtSecret)
+    const request = new HttpRequest(undefined, undefined, {}) 
 
-    const request = new HttpRequest(
-      undefined,
-      { authorization: `Bearer ${token}` },
-      {
-        studentRA: '23.00335-9',
-      },
-    )
+    const decoded = {
+      role: 'STUDENT',
+      ra: '23.00335-9',
+      name: 'Test User',
+    }
 
-    const response = await controller.handle(request)
+    const response = await controller.handle(request, decoded)
 
     expect(response?.statusCode).toEqual(400)
     expect(response?.body).toBe('Field notebookSerialNumber is missing')
@@ -52,43 +52,40 @@ describe('Assert Create Withdraw controller is correct at all', () => {
     const usecase = new CreateWithdrawUsecase(repo)
     const controller = new CreateWithdrawController(usecase)
 
-    const user = { name: 'Test User', studentRA: '23.00335-9' }
-    const token = jwt.sign({ user }, jwtSecret)
+    const request = new HttpRequest(undefined, undefined, {
+      notebookSerialNumber: 'ABC123',
+    })
 
-    const request = new HttpRequest(
-      undefined,
-      { authorization: `Bearer ${token}` },
-      {
-        notebookSerialNumber: 'ABC123',
-      },
-    )
+    const decoded = {
+      role: 'STUDENT',
+      ra: '',
+      name: 'Test User',
+    }
 
-    const response = await controller.handle(request)
+    const response = await controller.handle(request, decoded)
 
     expect(response?.statusCode).toEqual(400)
     expect(response?.body).toBe('Field studentRA is missing')
   })
 
-  it('Should throw 400 statusCode when studentRA is not valid', async () => {
+  it('Should throw 400 statusCode when not pass studentRA', async () => {
     const repo = new WithdrawRepositoryMock()
     const usecase = new CreateWithdrawUsecase(repo)
     const controller = new CreateWithdrawController(usecase)
 
-    const user = { name: 'Test User', studentRA: '23.00335-9' }
-    const token = jwt.sign({ user }, jwtSecret)
+    const request = new HttpRequest(undefined, undefined, {
+      notebookSerialNumber: 'ABC123',
+    })
 
-    const request = new HttpRequest(
-      undefined,
-      { authorization: `Bearer ${token}` },
-      {
-        notebookSerialNumber: 'ABC123',
-        studentRA: '22003359',
-      },
-    )
+    const decoded = {
+      role: 'STUDENT',
+      ra: undefined, 
+      name: 'Test User',
+    }
 
-    const response = await controller.handle(request)
+    const response = await controller.handle(request, decoded)
 
     expect(response?.statusCode).toEqual(400)
-    expect(response?.body).toBe('Invalid studentRA format')
+    expect(response?.body).toBe('Field studentRA is missing')
   })
 })
