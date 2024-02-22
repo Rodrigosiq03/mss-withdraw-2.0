1247462d63b89d3251c34df24fc43d3e18433efc
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-22 15:14:34 -0300

    adjusting IWithdrawRepository ✅

--- src/shared/domain/repositories/withdraw_repository_interface.ts
+++ src/shared/domain/repositories/withdraw_repository_interface.ts
@@ -1,7 +1,12 @@
 import { Withdraw } from '../entities/withdraw'
 
 export interface IWithdrawRepository {
-  createWithdraw(withdraw: Withdraw): Promise<Withdraw>
+  createWithdraw(
+    notebookSerialNumber: string,
+    studentRA: string,
+    name: string,
+    initTime: number,
+  ): Promise<Withdraw>
   getWithdrawByRA(ra: string): Promise<Withdraw>
   getAllWithdraws(): Promise<Withdraw[]>
   updateWithdrawByRA(ra: string, isChecked: boolean): Promise<Withdraw>
