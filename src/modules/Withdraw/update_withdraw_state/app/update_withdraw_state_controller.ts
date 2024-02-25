f9bdc3b78c90f9f7912c65db958b897d25056dbc
LucaPinheiro <lucapgomes11@gmail.com>
2024-02-25 01:54:24 -0300

    finishing update route✅

---
+++ src/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_controller.ts
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { UpdateWithdraw } from './update_withdraw_state_usecase'
+import { UpdateWithdrawViewModel } from './update_withdraw_state_viewmodel'
+import {
+  MissingParameters,
+  WrongTypeParameters,
+} from '../../../../shared/helpers/errors/controller_errors'
+import { EntityError } from '../../../../shared/helpers/errors/domain_errors'
+import { NoItemsFound } from '../../../../shared/helpers/errors/usecase_errors'
+import { IRequest } from '../../../../shared/helpers/external_interfaces/external_interface'
+import {
+  BadRequest,
+  InternalServerError,
+  NotFound,
+  OK,
+  Unauthorized,
+} from '../../../../shared/helpers/external_interfaces/http_codes'
+
+export class UpdateWithdrawController {
+  constructor(private usecase: UpdateWithdraw) {}
+
+  async handle(request: IRequest, user: any) {
+    try {
+      if (!user || (user.role !== 'EMPLOYEE' && user.role !== 'ADMIN')) {
+        throw new Unauthorized(
+          'Only employees or admins can perform this action',
+        )
+      }
+
+      if (request.data.notebookSerialNumber === undefined) {
+        throw new MissingParameters('notebookSerialNumber')
+      }
+      if (typeof request.data.notebookSerialNumber !== 'string') {
+        throw new WrongTypeParameters(
+          'notebookSerialNumber',
+          'string',
+          typeof request.data.notebookSerialNumber,
+        )
+      }
+      if (request.data.state === undefined) {
+        throw new MissingParameters('state')
+      }
+      if (typeof request.data.state !== 'boolean') {
+        throw new WrongTypeParameters(
+          'state',
+          'boolean',
+          typeof request.data.state,
+        )
+      }
+
+      const notebookSerialNumber = request.data.notebookSerialNumber
+      const updatedWithdraw = await this.usecase.execute(
+        notebookSerialNumber,
+        request.data.state as boolean,
+      )
+
+      const viewModel = new UpdateWithdrawViewModel(updatedWithdraw)
+      return new OK(viewModel.toJSON())
+    } catch (error: any) {
+      if (error instanceof NoItemsFound) {
+        return new NotFound(error.message)
+      }
+      if (
+        error instanceof MissingParameters ||
+        error instanceof WrongTypeParameters ||
+        error instanceof EntityError
+      ) {
+        return new BadRequest(error.message)
+      }
+      if (error instanceof Unauthorized) {
+        return new Unauthorized('This action is forbidden for only admins')
+      }
+      return new InternalServerError(error.message)
+    }
+  }
+}
