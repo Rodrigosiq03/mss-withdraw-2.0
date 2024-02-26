0ee5f749db1d1516d4162d465ac3d3150830ee17
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-02-26 18:34:27 -0300

    renamed usecase file to the sufix be "Usecase" ✅

--- src/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_usecase.ts
+++ src/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_usecase.ts
@@ -2,19 +2,19 @@ import { IWithdrawRepository } from '../../../../shared/domain/repositories/with
 import { EntityError } from '../../../../shared/helpers/errors/domain_errors'
 import { Withdraw } from '../../../../shared/domain/entities/withdraw'
 
-export class UpdateWithdraw {
+export class UpdateWithdrawUsecase {
   constructor(private repo: IWithdrawRepository) {}
 
-  async execute(ra: string, isChecked: boolean) {
-    if (!Withdraw.validateStudentRA(ra)) {
-      throw new EntityError('ra')
+  async execute(notebookSerialNumber: string, isChecked: boolean) {
+    if (!Withdraw.validateNotebookSerialNumber(notebookSerialNumber)) {
+      throw new EntityError('notebookSerialNumber')
     }
 
     if (typeof isChecked !== 'boolean' || isChecked === undefined) {
       throw new EntityError('isChecked')
     }
 
-    const withdraw = await this.repo.updateWithdrawByRA(ra, isChecked)
+    const withdraw = await this.repo.updateWithdrawByNotebookSerialNumber(notebookSerialNumber, isChecked)
 
     return withdraw
   }
