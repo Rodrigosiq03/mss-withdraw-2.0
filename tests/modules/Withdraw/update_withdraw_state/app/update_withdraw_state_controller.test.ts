3dd90cee2b17aa5f7f3d809a3aa215605f59a268
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-02-26 18:41:14 -0300

    added anew way to make the controller request and fixed al the messages of errors ✅

--- tests/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_controller.test.ts
+++ tests/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_controller.test.ts
@@ -1,28 +1,23 @@
 import { describe, it, expect } from 'vitest'
 import { UpdateWithdrawController } from '../../../../../src/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_controller'
-import { UpdateWithdraw } from '../../../../../src/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_usecase'
-import { IRequest } from '../../../../../src/shared/helpers/external_interfaces/external_interface'
+import { UpdateWithdrawUsecase } from '../../../../../src/modules/Withdraw/update_withdraw_state/app/update_withdraw_state_usecase'
 import {
   BadRequest,
   OK,
   Unauthorized,
 } from '../../../../../src/shared/helpers/external_interfaces/http_codes'
 import { WithdrawRepositoryMock } from '../../../../../src/shared/infra/repositories/withdraw_repository_mock'
+import { HttpRequest } from '../../../../../src/shared/helpers/external_interfaces/http_models'
 
 describe('UpdateWithdrawController Tests', () => {
   it('Should update withdraw state successfully when user is admin', async () => {
-    const request: IRequest = {
-      data: {
-        notebookSerialNumber: 'ABC123',
-        state: true,
-      },
-    }
+    const request = new HttpRequest({ notebookSerialNumber: 'MNO345', state: true })
     const user = {
-      role: 'ADMIN',
+      role: 'EMPLOYEE',
     }
 
     const repo = new WithdrawRepositoryMock()
-    const usecase = new UpdateWithdraw(repo)
+    const usecase = new UpdateWithdrawUsecase(repo)
     const controller = new UpdateWithdrawController(usecase)
 
     const response = await controller.handle(request, user)
@@ -33,89 +28,70 @@ describe('UpdateWithdrawController Tests', () => {
   })
 
   it('Should return Unauthorized when user is not authenticated', async () => {
-    const request: IRequest = {
-      data: {
-        notebookSerialNumber: 'ABC123',
-        state: true,
-      },
-    }
-    const user = null
+    const request = new HttpRequest({ notebookSerialNumber: 'MNO345', state: true })
+    
 
     const repo = new WithdrawRepositoryMock()
-    const usecase = new UpdateWithdraw(repo)
+    const usecase = new UpdateWithdrawUsecase(repo)
     const controller = new UpdateWithdrawController(usecase)
 
-    const response = await controller.handle(request, user)
+    const response = await controller.handle(request, undefined)
 
     expect(response).toBeInstanceOf(Unauthorized)
     expect(response?.body).toBe(
-      'This action is forbidden for only admins',
+      'The action is forbidden for this type of user',
     )
   })
 
   it('Should return Unauthorized when user is not admin or employee', async () => {
-    const request: IRequest = {
-      data: {
-        notebookSerialNumber: 'ABC123',
-        state: true,
-      },
-    }
+    const request = new HttpRequest({ notebookSerialNumber: 'MNO345', state: true })
     const user = {
       role: 'CUSTOMER',
     }
 
     const repo = new WithdrawRepositoryMock()
-    const usecase = new UpdateWithdraw(repo)
+    const usecase = new UpdateWithdrawUsecase(repo)
     const controller = new UpdateWithdrawController(usecase)
 
     const response = await controller.handle(request, user)
 
     expect(response).toBeInstanceOf(Unauthorized)
-    expect(response?.body).toBe('This action is forbidden for only admins')
+    expect(response?.body).toBe('The action is forbidden for this type of user')
   })
 
   it('Should return BadRequest when notebookSerialNumber is missing', async () => {
-    const request: IRequest = {
-      data: {
-        state: true,
-      },
-    }
+    const request = new HttpRequest({ state: true })
     const user = {
-      role: 'ADMIN',
+      role: 'EMPLOYEE',
     }
 
     const repo = new WithdrawRepositoryMock()
-    const usecase = new UpdateWithdraw(repo)
-    const controller = new UpdateWithdrawController(usecase)
-
-    const response = await controller.handle(request, user)
-
-    expect(response).toBeInstanceOf(BadRequest)
-    expect(expect(response?.body)).toBe(
-      'notebookSerialNumber parameter is missing',
-    )
-  })
-
-  it('Should return BadRequest when notebookSerialNumber is not a string', async () => {
-    const request: IRequest = {
-      data: {
-        notebookSerialNumber: 123,
-        state: true,
-      },
-    }
-    const user = {
-      role: 'ADMIN',
-    }
-
-    const repo = new WithdrawRepositoryMock()
-    const usecase = new UpdateWithdraw(repo)
+    const usecase = new UpdateWithdrawUsecase(repo)
     const controller = new UpdateWithdrawController(usecase)
 
     const response = await controller.handle(request, user)
 
     expect(response).toBeInstanceOf(BadRequest)
     expect(response?.body).toBe(
-      'notebookSerialNumber parameter must be a string',
+      'Field notebookSerialNumber is missing',
+    )
+  })
+
+  it('Should return BadRequest when notebookSerialNumber is not a string', async () => {
+    const request = new HttpRequest({ notebookSerialNumber: 123, state: true })
+    const user = {
+      role: 'ADMIN',
+    }
+
+    const repo = new WithdrawRepositoryMock()
+    const usecase = new UpdateWithdrawUsecase(repo)
+    const controller = new UpdateWithdrawController(usecase)
+
+    const response = await controller.handle(request, user)
+
+    expect(response).toBeInstanceOf(BadRequest)
+    expect(response?.body).toBe(
+      'Field notebookSerialNumber isn\'t in the right type.\n Received: number.\n Expected: string.',
     )
   })
 })
