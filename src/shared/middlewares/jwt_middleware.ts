398ce8687aa8d6f05eb439b420c921c0d68d8af9
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-15 23:57:22 -0300

    fix: enhance error handling in getUserFromToken for missing authorization and token

--- src/shared/middlewares/jwt_middleware.ts
+++ src/shared/middlewares/jwt_middleware.ts
@@ -2,15 +2,37 @@
 import jwt from 'jsonwebtoken'
 import { EntityError } from '../helpers/errors/domain_errors'
 
+// export function getUserFromToken(authorization: string) {
+//   const token = authorization.split(' ')[1]
+
+//   console.log('token', token)
+
+//   const decoded = jwt.decode(token) as any
+//   if (!decoded) {
+//     throw new EntityError('token')
+//   }
+
+//   return decoded.email
+// }
+
+
 export function getUserFromToken(authorization: string) {
-  const token = authorization.split(' ')[1]
-
-  console.log('token', token)
-
-  const decoded = jwt.decode(token) as any
-  if (!decoded) {
-    throw new EntityError('token')
+  if (!authorization) {
+    throw new EntityError('Authorization header is missing');
   }
 
-  return decoded.email
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    throw new EntityError('Bearer token is missing');
+  }
+
+  console.log('Token:', token);
+
+  const decoded = jwt.decode(token) as any;
+  if (!decoded || !decoded.user) {
+    throw new EntityError('Invalid token: user not found');
+  }
+
+  return decoded.user; // Retorna o objeto `user` completo
 }
+
